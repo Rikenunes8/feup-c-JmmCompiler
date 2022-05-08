b0a12c6b573bfe750e7a13bf1d733daad06458c7
Patricia Oliveira <up201905427@fe.up.pt>
2022-05-08 16:54:26 +0100

    First draft to generate code to each type of main instruction that can be found in a method

--- src/pt/up/fe/comp/jasmin/OllirToJasmin.java
+++ src/pt/up/fe/comp/jasmin/OllirToJasmin.java
@@ -2,6 +2,7 @@ package pt.up.fe.comp.jasmin;
 
 import org.specs.comp.ollir.*;
 import pt.up.fe.comp.jmm.report.Report;
+import pt.up.fe.specs.util.classmap.FunctionClassMap;
 import pt.up.fe.specs.util.exceptions.NotImplementedException;
 
 import java.util.ArrayList;
@@ -13,17 +14,23 @@ public class OllirToJasmin {
     private final ClassUnit classUnit;
     private final List<Report> reports;
 
+    private final FunctionClassMap<Instruction, String> instructionMap;
+
     public OllirToJasmin(ClassUnit classUnit) {
         this.classUnit = classUnit;
         this.reports = new ArrayList<>();
-    }
 
-    // construct jasmin code for:
-    // 1. function calls*
-    // 2. arithmetic expression
-    // 3. if the else commands
-    // 4. assignments*
-    // 5. command sequences*
+        this.instructionMap = new FunctionClassMap<>();
+
+        this.instructionMap.put(AssignInstruction.class, this::getJasminCode);
+        this.instructionMap.put(CallInstruction.class, this::getJasminCode);
+        this.instructionMap.put(GetFieldInstruction.class, this::getJasminCode);
+        this.instructionMap.put(OpInstruction.class, this::getJasminCode);
+        this.instructionMap.put(SingleOpInstruction.class, this::getJasminCode);
+        this.instructionMap.put(CondBranchInstruction.class, this::getJasminCode);
+        this.instructionMap.put(GotoInstruction.class, this::getJasminCode);
+        this.instructionMap.put(ReturnInstruction.class, this::getJasminCode);
+    }
 
     public List<Report> getReports() {
         return this.reports;
@@ -32,34 +39,26 @@ public class OllirToJasmin {
     public String getJasminCode() {
         StringBuilder jasminCode = new StringBuilder();
 
-        jasminCode.append(this.getInitClassCode());
+        String extendedClass = (this.classUnit.getSuperClass() == null) ?
+                "java/lang/Object" : this.getFullyQualifiedClassName(this.classUnit.getSuperClass());
+
+        jasminCode.append(".class public ").append(this.classUnit.getClassName()).append("\n");
+        jasminCode.append(".super ").append(extendedClass).append("\n");
+
+        if (!this.classUnit.getFields().isEmpty()) jasminCode.append("\n");
+        for (Field field : this.classUnit.getFields()) {
+            jasminCode.append(this.getJasminCode(field));
+        }
+
+        jasminCode.append(this.getJasminConstructorCode(extendedClass));
 
         for (Method method : this.classUnit.getMethods()) {
             if (!method.isConstructMethod()) {
-                jasminCode.append(this.getMethodCode(method));
+                jasminCode.append(this.getJasminCode(method));
             }
         }
 
         return jasminCode.toString();
-    }
-
-    private String getInitClassCode() {
-        StringBuilder code = new StringBuilder();
-
-        String extendedClass = (this.classUnit.getSuperClass() == null) ?
-                "java/lang/Object" : this.getFullyQualifiedClassName(this.classUnit.getSuperClass());
-
-        code.append(".class public ").append(this.classUnit.getClassName()).append("\n");
-        code.append(".super ").append(extendedClass).append("\n");
-
-        if (!this.classUnit.getFields().isEmpty()) code.append("\n");
-        for (Field field : this.classUnit.getFields()) {
-            code.append(this.getFieldCode(field));
-        }
-
-        code.append(this.getJasminConstructorCode(extendedClass));
-
-        return code.toString();
     }
 
     private String getFullyQualifiedClassName(String className) {
@@ -75,7 +74,7 @@ public class OllirToJasmin {
         return extendImport.get(0).replace('.', '/');
     }
 
-    private String getFieldCode(Field field) {
+    public String getJasminCode(Field field) {
         StringBuilder code = new StringBuilder();
 
         String accessAnnotation = field.getFieldAccessModifier() == AccessModifiers.DEFAULT ?
@@ -97,7 +96,7 @@ public class OllirToJasmin {
                 ".end method\n";
     }
 
-    private String getMethodCode(Method method) {
+    public String getJasminCode(Method method) {
         StringBuilder code = new StringBuilder();
 
         // Method header
@@ -118,7 +117,7 @@ public class OllirToJasmin {
 
         // Method Instructions
         for (Instruction instruction : method.getInstructions()) {
-            code.append(this.getMethodInstructionCode(method, instruction));
+            code.append(this.getJasminCode(instruction));
         }
 
         // Method End
@@ -136,8 +135,48 @@ public class OllirToJasmin {
         // NOTE: Now we can use 99, but this will be changed for checkpoint 3
     }
 
-    private String getMethodInstructionCode(Method method, Instruction instruction) {
+
+    // construct jasmin code for:
+    // 1. function calls*
+    // 2. arithmetic expression
+    // 3. if the else commands
+    // 4. assignments*
+    // 5. command sequences*
+
+    public String getJasminCode(Instruction instruction) {
+        return this.instructionMap.apply(instruction);
+    }
+
+    public String getJasminCode(AssignInstruction instruction) {
+        throw new NotImplementedException(instruction.getInstType());
+    }
+
+    public String getJasminCode(CallInstruction instruction) {
         return "";
+    }
+
+    public String getJasminCode(GetFieldInstruction instruction) {
+        throw new NotImplementedException(instruction.getInstType());
+    }
+
+    public String getJasminCode(OpInstruction instruction) {
+        throw new NotImplementedException(instruction.getInstType());
+    }
+
+    public String getJasminCode(SingleOpInstruction instruction) {
+        throw new NotImplementedException(instruction.getInstType());
+    }
+
+    public String getJasminCode(CondBranchInstruction instruction) {
+        throw new NotImplementedException(instruction.getInstType());
+    }
+
+    public String getJasminCode(GotoInstruction instruction) {
+        throw new NotImplementedException(instruction.getInstType());
+    }
+
+    public String getJasminCode(ReturnInstruction instruction) {
+        throw new NotImplementedException(instruction.getInstType());
     }
 
     private String getJasminType(Type type) {
