5caca006eb6864162349987a2b0984fcca683967
Henrique Nunes <hrn2001@gmail.com>
2022-06-05 00:46:28 +0100

    Solve semantic tests not passing
    Missing a test but it is not our fault (the variable "c" should be initialized before return statement)

--- src/pt/up/fe/comp/analysis/JmmAnalyser.java
+++ src/pt/up/fe/comp/analysis/JmmAnalyser.java
@@ -35,6 +35,7 @@ public class JmmAnalyser implements JmmAnalysis {
 
         List<SemanticAnalyserVisitor> anlysers = Arrays.asList(
                 new SymbolTableVisitor(),
+                new FieldsInStaticMethods(),
                 new UndefinedVarsVisitor(),
                 new CheckImportsVisitor(),
                 new TypeCheckingVisitor(),
