100972bcce874de774b397de44979fa3424781e2
Patricia Oliveira <up201905427@fe.up.pt>
2022-06-06 23:38:46 +0100

    Compute limit locals, correct test templates

--- src/pt/up/fe/comp/jasmin/JasminGenerator.java
+++ src/pt/up/fe/comp/jasmin/JasminGenerator.java
@@ -123,9 +123,10 @@ public class JasminGenerator {
     private String getMethodLimitsCode(Method method) {
         // limit stack - max length of the stack that we need to the method
         // limit locals - max number of registers we need to use
+        int locals = method.getVarTable().size() + 1;
 
         return "\t.limit stack " + 99 + "\n" +
-                "\t.limit locals " + 99 + "\n\n";
+                "\t.limit locals " + locals + "\n\n";
         // NOTE: Now we can use 99, but this will be changed for checkpoint 3
     }
 
