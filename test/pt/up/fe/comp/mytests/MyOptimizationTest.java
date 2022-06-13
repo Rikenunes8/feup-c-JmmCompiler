661aca8f94d7e7b45402f3eac9461872e5b8948c
Henrique Nunes <hrn2001@gmail.com>
2022-06-13 14:04:10 +0100

    Fix failing test

--- test/pt/up/fe/comp/mytests/MyOptimizationTest.java
+++ test/pt/up/fe/comp/mytests/MyOptimizationTest.java
@@ -3,6 +3,7 @@ package pt.up.fe.comp.mytests;
 import org.junit.Test;
 import pt.up.fe.comp.CpUtils;
 import pt.up.fe.comp.TestUtils;
+import pt.up.fe.comp.jmm.ollir.JmmOptimization;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
 import pt.up.fe.comp.ollir.JmmOptimizer;
 import pt.up.fe.specs.util.SpecsIo;
@@ -12,20 +13,31 @@ import java.util.*;
 import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
+import static pt.up.fe.comp.TestUtils.analyse;
 
 public class MyOptimizationTest {
     static OllirResult getOllirResult(String filename) {
         return TestUtils.optimize(SpecsIo.getResource("fixtures/public/optimizations/" + filename));
     }
-    static OllirResult getOllirResultOpt(String filename) {
+    static OllirResult getOllirResultOpt(String filename, int type) { // 0 - all; 1 - ast; 2 - ollir
         Map<String, String> config = new HashMap<>();
         config.put("optimize", "true");
-        return TestUtils.optimize(SpecsIo.getResource("fixtures/public/optimizations/" + filename), config);
+
+        String jmmCode = SpecsIo.getResource("fixtures/public/optimizations/" + filename);
+        var semanticsResult = TestUtils.analyse(jmmCode, config);
+        TestUtils.noErrors(semanticsResult.getReports());
+
+        JmmOptimization optimization = TestUtils.getJmmOptimization();
+        if (type != 2) semanticsResult = optimization.optimize(semanticsResult);
+        var ollirResult = optimization.toOllir(semanticsResult);
+        if (type != 1) ollirResult = optimization.optimize(ollirResult);
+
+        return ollirResult;
     }
 
-    static OllirResult getSetup(String filename) {
+    static OllirResult getSetup(String filename, int type) { // type: 0 - all; 1 - ast; 2 - ollir
         var ollirResult = getOllirResult(filename);
-        var ollirResultOpt = getOllirResultOpt(filename);
+        var ollirResultOpt = getOllirResultOpt(filename, type);
         TestUtils.noErrors(ollirResult.getReports());
         TestUtils.noErrors(ollirResultOpt.getReports());
         assertNotEquals(ollirResult.getOllirCode(), ollirResultOpt.getOllirCode());
@@ -34,7 +46,7 @@ public class MyOptimizationTest {
 
     @Test
     public void constant_propagation_while() {
-        String ollirCode = getSetup("while.jmm").getOllirCode();
+        String ollirCode = getSetup("while.jmm", 1).getOllirCode();
 
         String cond = ".*a\\.i32 <\\.bool 4\\.i32.*";
         String assignment = ".*c\\.i32 :=\\.i32 4\\.i32.*";
@@ -44,7 +56,7 @@ public class MyOptimizationTest {
 
     @Test
     public void constant_propagation_while_nested() {
-        String ollirCode = getSetup("while_nested.jmm").getOllirCode();
+        String ollirCode = getSetup("while_nested.jmm", 1).getOllirCode();
 
         String cond1 = ".*a\\.i32 <\\.bool 4\\.i32.*";
         String cond2 = ".*c\\.i32 <\\.bool 4\\.i32.*";
@@ -64,19 +76,9 @@ public class MyOptimizationTest {
         assertTrue(SpecsStrings.matches(ollirCode, Pattern.compile(assignment6)));
     }
 
-    @Test // TODO do something to this test latter
-    public void register_allocation() {
-        String ollirCode = SpecsIo.getResource("fixtures/public/temp.ollir");
-        JmmOptimizer optimizer = new JmmOptimizer();
-        Map<String, String> config = new HashMap<>();
-        config.put("registerAllocation", String.valueOf(5));
-        OllirResult ollirResult = new OllirResult(ollirCode, config);
-        optimizer.optimize(ollirResult);
-    }
-
     @Test
     public void dead_code_elimination_if_true() {
-        String ollirCode = getSetup("if_true.jmm").getOllirCode();
+        String ollirCode = getSetup("if_true.jmm", 1).getOllirCode();
 
         String assignment1 = "b.i32 :=.i32 1.i32";
         String assignment2 = "b.i32 :=.i32 2.i32";
@@ -89,7 +91,7 @@ public class MyOptimizationTest {
 
     @Test
     public void dead_code_elimination_if_false() {
-        String ollirCode = getSetup("if_false.jmm").getOllirCode();
+        String ollirCode = getSetup("if_false.jmm", 1).getOllirCode();
 
         String assignment1 = "b.i32 :=.i32 1.i32";
         String assignment2 = "b.i32 :=.i32 2.i32";
@@ -103,7 +105,7 @@ public class MyOptimizationTest {
 
     @Test
     public void dead_code_elimination_while() {
-        String ollirCode = getSetup("while_false.jmm").getOllirCode();
+        String ollirCode = getSetup("while_false.jmm", 1).getOllirCode();
         String assignment1 = "a.i32 :=.i32 0.i32";
         String assignment2 = "a.i32 :=.i32 a.i32 +.i32 1.i32";
         String returnV = "ret.V";
@@ -117,7 +119,7 @@ public class MyOptimizationTest {
 
     @Test
     public void dead_code_elimination_while_nested() {
-        String ollirCode = getSetup("while_nested_false.jmm").getOllirCode();
+        String ollirCode = getSetup("while_nested_false.jmm", 1).getOllirCode();
 
         String assignment1 = "a.i32 :=.i32 1.i32";
         String assignment2 = "b.i32 :=.i32 1.i32";
@@ -130,5 +132,15 @@ public class MyOptimizationTest {
 
     public static int countOccurences(String code, String word) {
         return (code.length() - code.replace(word, "").length()) / word.length();
+    }
+
+    @Test // TODO do something to this test latter
+    public void register_allocation() {
+        String ollirCode = SpecsIo.getResource("fixtures/public/temp.ollir");
+        JmmOptimizer optimizer = new JmmOptimizer();
+        Map<String, String> config = new HashMap<>();
+        config.put("registerAllocation", String.valueOf(5));
+        OllirResult ollirResult = new OllirResult(ollirCode, config);
+        optimizer.optimize(ollirResult);
     }
 }
